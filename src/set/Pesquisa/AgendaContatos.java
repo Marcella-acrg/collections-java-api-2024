7d79b1f8cde210c40340d1c55936b41d9306b7f6
Marcella-acrg <marcellaacoelho@gmail.com>
2024-06-22 17:03:17 -0300

    Resolvendo exercicios ConnjuntoConvidado, CadastroProdutos e AgendaContatos
    Signed-off-by: Marcella-acrg <marcellaacoelho@gmail.com>

---
+++ src/set/Pesquisa/AgendaContatos.java
@@ -0,0 +1,62 @@
+package set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    //atributo
+    private Set<Contato> contatoset;
+
+    public AgendaContatos() {
+        this.contatoset = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero){
+        contatoset.add(new Contato(nome, numero));
+    }
+
+    public void exibirContatos(){
+        System.out.println(contatoset);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome){
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for(Contato c : contatoset ){
+            if(c.getNome().startsWith(nome)){
+                contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+        for(Contato c : contatoset) {
+            if(c.getNome().equalsIgnoreCase(nome)) {
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+        agendaContatos.exibirContatos();
+
+        agendaContatos.adicionarContato("Camila", 123456);
+        agendaContatos.adicionarContato("Camila", 5665);
+        agendaContatos.adicionarContato("Camila Cavalcante", 1111111);
+        agendaContatos.adicionarContato("Camila Dio", 654987);
+        agendaContatos.adicionarContato("Maria Silva", 1111111);
+
+        agendaContatos.exibirContatos();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Maria"));
+
+        System.out.println("Contato atualizado: " + agendaContatos.atualizarNumeroContato("Maria Silva", 5555555));
+
+        agendaContatos.exibirContatos();
+    }
+}
